de6a7144b274aafa306f83e5e4e884a502652763
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-04-28 22:45:31 +0100

    love

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -14,8 +14,10 @@ type ContextValue = {
   clearAddOn: () => void;
 };
 
+// 1
 const AppContext = createContext<ContextValue>({} as ContextValue);
 
+// 2
 const AppProvider = ({ children }: AppProviderProps) => {
   const [addsOn, setAddsOn] = useState<obj[]>([]);
 
@@ -45,8 +47,11 @@ const AppProvider = ({ children }: AppProviderProps) => {
   );
 };
 
+// 3
+
 export const useAppContext = () => {
   return useContext(AppContext);
 };
 
+// 4
 export default AppProvider;
---
+++ src/context/context2.tsx
@@ -0,0 +1,12 @@
+// import { createContext, useState, useEffect } from "react";
+
+// // 1
+// const DataContext = createContext({});
+
+// // 2
+// export const DataProvider = ({ children }) => {
+//   return <DataContext.Provider value={{}}>{children}</DataContext.Provider>;
+// };
+
+// // 3
+// export default DataContext;
